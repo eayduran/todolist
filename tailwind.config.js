761aeb9e0322405eb9a486a2f0f6f531117d83f3
Enes Ayduran <ayduran@Eness-MacBook-Pro.local>
2022-08-17 12:19:36 +0300

    fix general design

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'gray230' : 'rgb(230,230,230)'
+      }
+    },
   },
   plugins: [],
 }
